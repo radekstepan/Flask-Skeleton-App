22f13bc098468ee1b799313ff3fde0087614a145
Radek Stepan <radek.stepan@gmail.com>
2011-08-06 19:24:19 +0200

    check if unicode

--- libs/utils.py
+++ libs/utils.py
@@ -4,6 +4,7 @@
 def slugify(value):
     import unicodedata, re
 
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    if isinstance(value, unicode):
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
     return re.sub('[-\s]+', '-', value)
